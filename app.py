45255a4475f78356450b73c33ff0ee35fb159235
lihua01 <363088847@qq.com>
2019-12-01 21:16:32 +0800

    first

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
-birth_year = input('Birth year: ')
-age = 2019 - int(birth_year)
-print(age)
+# birth_year = input('Birth year: ')
+# age = 2019 - int(birth_year)
+# print(age)
+# weight_lbs = input('Weight (lbs): ')
+# weight_kg = int(weight_lbs)*0.45
+# print(weight_kg)
+course ='Python for Beginners'
+
+print(course[1:-1])
