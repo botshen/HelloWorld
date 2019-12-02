e346ffb26d750b79d1512b5274d2af3ef5d363ab
lihua01 <363088847@qq.com>
2019-12-02 20:43:17 +0800

    third

--- app.py
+++ app.py
@@ -5,9 +5,6 @@
 # weight_kg = int(weight_lbs)*0.45
 # print(weight_kg)
 course ='Python for Beginners'
-
-print(course[1:-1])
-print("bot")
-print("1")
-name = "bot"
-print(f'{name} is my name')
+print(len(course))
+print(course.upper())
+print(course.replace('for','FOR'))
